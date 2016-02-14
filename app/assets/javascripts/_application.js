355332f2ff982002e262ca7b24a746a965e74068
Nathan <nathanrbourke@gmail.com>
2016-02-13 22:05:19 -0500

    react and jquery installed, restructure where webpack gets its sources for transpiling

--- app/assets/javascripts/_application.js
+++
@@ -1,3 +0,0 @@
-// This file, as well as everything require here,
-//  is transpiled through webpack
-require('./init.js')
---
+++ client/application.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import $ from 'jquery';
+import {render} from 'react-dom';
+import Test from './test.js';
+
+
+// import TopLevelReactComponent Here
+
+// call top level react component here
